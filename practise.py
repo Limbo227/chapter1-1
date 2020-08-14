62ed853bd29098dbe96d89f4b0a54f596f4c0769
Limbo228 <your_email@example.com>
2020-08-14 11:43:04 +0600

    chapter1,1

---
+++ practise.py
@@ -0,0 +1,31 @@
+# #1
+# index = 0
+# a = [1,2,10,7,8,4,2,3]
+# print(a [0])
+# print(a [7])
+# #2
+# a = ['Усейн Болт,Барри Аллен,Майк Тайссон']
+# name = input('please input a name')
+# if name == ('Усейн Болт'):
+#     print('First place')
+# elif name == ('Барри Аллен'):
+#     print('Second place')
+# elif name == ('Майк Тайсон'):
+#     print ('Third Place')
+# #3
+# a = ["Bakai,Bogdan,Arsen,Kirill"]
+# name = input('input name')
+# a.insert(0,name)
+# print(a)
+# #4
+# a = ['Картошка, Морковь, Мясо, Сыр,']
+# a.append("Яблоки Молоко Клубника")
+# print(a)
+# #5
+# a = {'name':'John', 'last_name':'Snow', 'age':20, 'salary':100}
+# python = input('')
+# a['salary'] = python
+# print(a)
+# #6
+# items = ['pen,pencil,laptop,phone,']
+# print(items)
---
+++ test.py
@@ -0,0 +1,55 @@
+# #1
+# num1 = int(input('input a num1'))
+# num2 = int(input('input num2'))
+# print(num1 - num2)
+# #2
+# print(36%5)
+# #3
+# word = input('input слово')
+#
+# if word == word[::-1]:
+#     print('correct')
+# else:
+#     print('wrong')
+# # #4
+# love = 'i love java'
+# print(love[:6], 'python')
+# #5
+# name=input('input name')
+# print(name*10)
+# #6
+# text = str(input('please input a text'))[::-1]
+# print(text)
+# #7
+# num=input('input num')
+# if len(num) >=10:
+#     print(num[9])
+# elif len(num) <10:
+#     numlen= num * 10
+#     print(numlen[9])
+# #8
+# txt = str(input('input txt'))
+# if len(txt) > 2:
+#     print(txt[:2], txt[-2:])
+# elif len(txt) == 2:
+#     print(txt)
+# elif len(txt) < 2:
+#     print("ОШИБКА ПРИРОДЫ")
+# #9
+# num = int(input('input num'))
+# print(num - 1, num + 1)
+# #10
+# num1 = int(input('input num1'))
+# num2 = int(input('input num2'))
+# if num1>num2:
+#     print(num1, '>', num2)
+# else:
+#     print(num2, '>', num1)
+# #11
+# num = int(input('input num'))
+# if num == 0:
+#     print('нуль')
+# elif num > 0:
+#     print('положительная')
+# else:
+#     print('отрицательная')
